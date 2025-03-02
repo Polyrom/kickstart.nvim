f5d455e134b47a95ce23a6545e8c2d928988119d
Roman Poliakov <roman61394@gmail.com>
2025-03-03 02:25:00 +0300

    add code format keymap

--- init.lua
+++ init.lua
@@ -400,6 +400,9 @@ require('lazy').setup({
           --  Useful when your language has ways of declaring types without an actual implementation.
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
+          -- format code
+          map('<leader>cf', vim.lsp.buf.format, '[C]ode [F]ormat')
+
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
